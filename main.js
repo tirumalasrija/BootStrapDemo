3c184d46f494af95264e3284ba1d728e10e50897
Srija <tirumalasri9@gmail.com>
2020-02-25 20:40:14 -0500

    JavaScript
    JavaScript changes

---
+++ main.js
@@ -0,0 +1,39 @@
+// alert("hello");
+// var hi="srija";
+// var a=3;
+// var b=30;
+// console.log(a*b,hi );
+
+// function validate(x,y) {
+//     result=(x*y);
+// }
+// validate(20, 40);
+// console.log(result);
+
+// const name="srija";
+// function validate() {
+//     husName="sai";
+// }
+// validate();
+// console.log(husName);
+// var name="chicken";
+//
+// if(name === "srija") {
+//     console.log("name is equal");
+// }else{
+//     console.log("not equal");
+// }
+function validate() {
+var enterName =document.getElementById("nameSrija").value;
+if(enterName ===""){
+    document.getElementById("nameError").style.display="block";
+}
+var enterFullName=document.getElementById("fullName").value;
+
+    if(enterName ==="" || enterFullName===""){
+        alert("Please fill the form");
+    }else{
+        alert("thank you for submitting the form");
+    }
+}
+
